14c5b8d1e55d8c32f4bb79c09c974cccebb2631f
markjrieke <markjrieke@gmail.com>
2025-03-08 11:00:56 -0600

    setup men/women tourney pages

---
+++ projects/2025-march-madness/render_tournament.R
@@ -0,0 +1,20 @@
+# setup ------------------------------------------------------------------------
+
+leagues <- c("mens", "womens")
+
+for (league in leagues) {
+  
+  file.copy(
+    from = "projects/2025-march-madness/_template.qmd",
+    to = glue::glue("projects/2025-march-madness/{league}.qmd"),
+    overwrite = TRUE
+  )
+  
+  quarto::quarto_render(
+    input = glue::glue("projects/2025-march-madness/{league}.qmd"),
+    execute_params = list(league = league)
+  )
+  
+}
+
+
